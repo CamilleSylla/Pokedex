265da75a9cd1404f0e49c9e9d63c161b8d22f8e7
CamilleSylla <camillesylla.web@gmail.com>
2020-07-01 11:43:20 +0200

    added PokeList js and css

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,18 @@
 import React, { Component } from 'react';
+import './style/App.css';
 
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state ={};
+  }
+
+  render () {
+    return(
+      <div className="App">
+
+      </div>
+    )
+  }
+}
